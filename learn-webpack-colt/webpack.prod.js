7ea4b5a74073041f5f2b75d46fde53b4b559ddba
Francis Calizo <francisacalizo@gmail.com>
2020-01-22 16:38:04 -0500

    Add html-loader, file-loader, and clean-webpack-plugin

--- learn-webpack-colt/webpack.prod.js
+++ learn-webpack-colt/webpack.prod.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge'); // allows us to combine webpack configs
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // cleans the dist folder of unused hashed files upon build
 
 module.exports = merge(common, {
   mode: 'production',
-  entry: './src/index.js',
   output: {
     filename: 'main[contentHash].js', // [contentHash] to prevent a users browser from cacheing
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new CleanWebpackPlugin()]
 });
