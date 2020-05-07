4c77a3960a97ef9bd966948e342bf2f973fbd936
Francis Calizo <francisacalizo@gmail.com>
2020-05-07 16:25:30 -0400

    Finish Quick tutorial

---
+++ ts-in-five-minutes/classes.ts
@@ -0,0 +1,25 @@
+class Student {
+  fullName: string;
+
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public middleInitial?: string
+  ) {
+    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  middleInitial?: string;
+}
+
+function greeter(person: Person) {
+  return `Hello ${person.firstName} ${person.lastName}`;
+}
+
+let user = new Student('Francis', 'Smith');
+
+console.log(greeter(user));
