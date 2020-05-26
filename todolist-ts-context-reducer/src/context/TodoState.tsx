b6963fa5316a8d70ec712ae9c48d0acf7e5b6e9e
Francis Calizo <francisacalizo@gmail.com>
2020-05-26 12:17:46 -0400

    Setup context

---
+++ todolist-ts-context-reducer/src/context/TodoState.tsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import TodoContext from './todoContext'
+
+// interface Props {
+//   children: React.r
+// }
+
+// export default ({children}) => {
+//   return (
+//     <TodoContext.Provider value={{
+//       todo
+//     }}>
+//       {children}
+//     </TodoContext.Provider>
+//   )
+// }
+export const placeholder = 'placeholder';
