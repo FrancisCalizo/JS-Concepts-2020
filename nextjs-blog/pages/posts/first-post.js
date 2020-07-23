c949e1e1ccbc938916565e264b59edb56b3b9ee0
Francis Calizo <francisacalizo@gmail.com>
2020-07-23 15:25:29 -0400

    Assets, metadata, and css Nextjs 3.8

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,15 +1,22 @@
+import Head from 'next/head';
 import Link from 'next/link';
+
+import Layout from '../../components/layout';
 
 const firstPost = () => {
   return (
-    <div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to Home Page</a>
         </Link>
       </h2>
-    </div>
+    </Layout>
   );
 };
 
