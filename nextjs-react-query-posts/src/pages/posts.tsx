06b3524e4aa3e689fc88e50ebacf5efb08170876
Francis Calizo <francisacalizo@gmail.com>
2020-08-07 22:27:59 -0400

    implement useQuery with getServerSideProps

--- nextjs-react-query-posts/src/pages/posts.tsx
+++ nextjs-react-query-posts/src/pages/posts.tsx
@@ -1,5 +1,6 @@
 import Layout from '../components/layout';
 import { GetServerSideProps } from 'next';
+import { useQuery } from 'react-query';
 
 import { Post } from '../types/index';
 
@@ -7,7 +8,18 @@ type Props = {
   posts: Post[];
 };
 
-export default function Posts({ posts }: Props) {
+const getTodos = async () => {
+  const res = await fetch('http://localhost:3001/posts');
+  const data = await res.json();
+
+  return data;
+};
+
+export default function Posts(props: Props) {
+  const { data: posts, isFetching } = useQuery('todos', getTodos, {
+    initialData: props.posts,
+  });
+
   return (
     <Layout title="Posts">
       <h1>Posts</h1>
